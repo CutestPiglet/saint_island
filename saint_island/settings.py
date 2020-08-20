5e072e67c603600d3009fda9c346e161fad1a9fd
CutestPiglet <johnmay0629@gmail.com>
2020-08-20 08:13:13 +0000

    [Heroku] Add Heroku settings

--- saint_island/settings.py
+++ saint_island/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'p@y_xs3y1jn#o#l21_jo-nf232)@%#&(#j!%65g!&by-j)bg+n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', True)
+DEBUG = bool(int(os.environ.get('DJANGO_DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'saint_island.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'))}
 
 
 # Password validation
