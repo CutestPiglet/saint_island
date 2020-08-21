0cb5e18d0dbe13ee7ae13c2f2ad8aa38e7159045
CutestPiglet <johnmay0629@gmail.com>
2020-08-21 02:46:32 +0000

    Add upload file button

--- chufang/views.py
+++ chufang/views.py
@@ -1,7 +1,15 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
+import shortuuid
 
 
 @staff_member_required
 def index(request):
+
+    if request.method == 'POST':
+        uploaded_file = request.FILES.get('uploaded-file')
+        if uploaded_file:
+            for line_data in uploaded_file.readlines():
+                pass
+
     return render(request, 'chufang/index.html')
